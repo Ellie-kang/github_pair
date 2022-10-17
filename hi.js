a0e51852fe070e3100b724fbb0afa829b5fdffb0
Ellie-kang <kya6873@hanmail.net>
2022-10-17 22:29:06 +0900

    add

--- hi.js
+++ hi.js
@@ -2,6 +2,20 @@ function hi() {
   console.log("hi");
 }
 
+<<<<<<< HEAD
 function hi2() {
   console.log("hi2");
 }
+=======
+  function hi2() {
+    console.log("hi2");
+  }
+
+  function hi3() {
+    console.log("hi3");
+  }
+
+  function hi4() {
+    console.log("hi4");
+  }
+>>>>>>> 11e3748 (add)
